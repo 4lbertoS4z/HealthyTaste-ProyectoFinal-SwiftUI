2911cf9f61d0858bb83ed949beebffe396163aeb
Alberto Saz <85312043+4lbertoS4z@users.noreply.github.com>
2023-11-01 17:06:03 +0100

    Add files via upload

---
+++ Prototipo HealthyTaste/Prototipo HealthyTaste/ContentView.swift
@@ -0,0 +1,29 @@
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            FirstDishListView()
+                .tabItem {
+                    Label("Entrantes", systemImage: "fork.knife")
+                }
+            
+            SecondDishListView()
+                .tabItem {
+                    Label("Segundos", systemImage: "fork.knife")
+                }
+            
+            DessertDishListView()
+                .tabItem {
+                    Label("Postres", systemImage: "fork.knife")
+                }
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
